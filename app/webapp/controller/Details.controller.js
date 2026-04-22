9919db24cfa6b67fadee3f2364827f34701aeac4
Richard Musime <richardmusime6@gmail.com>
2026-04-22 10:52:45 +0200

    Feat: added message box to confirm before renting

--- app/webapp/controller/Details.controller.js
+++ app/webapp/controller/Details.controller.js
@@ -1,9 +1,10 @@
 sap.ui.define(
     [
         "com/moyo/demo/caplugins/controller/BaseController",
-        "sap/m/MessageToast"
+        "sap/m/MessageToast",
+        "sap/m/MessageBox"
     ],
-    function (BaseController, MessageToast) {
+    function (BaseController, MessageToast, MessageBox) {
         "use strict";
 
         return BaseController.extend("com.moyo.demo.caplugins.controller.Details", {
@@ -37,36 +38,47 @@ sap.ui.define(
                 const oView = this.getView();
                 const oContext = oView.getBindingContext();
                 const sCarID = oContext.getProperty("ID");
+                const sBrand = oContext.getProperty("brand");
+                const sModel = oContext.getProperty("model");
                 const oModel = this.getModel();
 
-                oView.setBusy(true);
+                MessageBox.confirm(
+                    "Are you sure you want to rent the " + sBrand + " " + sModel + "?",
+                    {
+                        title: "Confirm Rental",
+                        actions: [MessageBox.Action.YES, MessageBox.Action.NO],
+                        emphasizedAction: MessageBox.Action.YES,
+                        onClose: function (sAction) {
+                            if (sAction === MessageBox.Action.YES) {
+                                oView.setBusy(true);
 
-                const oAction = oModel.bindContext("/rentCar(...)");
-                oAction.setParameter("carID", sCarID);
+                                const oAction = oModel.bindContext("/rentCar(...)");
+                                oAction.setParameter("carID", sCarID);
 
-                oAction.execute().then(
-                    function () {
-                        oView.setBusy(false);
-                        MessageToast.show("Car rented successfully!");
-                        oContext.refresh();
-                    }.bind(this),
-                    function (oError) {
-                        oView.setBusy(false);
-                        MessageToast.show("Error renting car: " + oError.getMessage());
-                    }.bind(this)
+                                oAction.execute().then(
+                                    function () {
+                                        oView.setBusy(false);
+                                        MessageToast.show(sBrand + " " + sModel + " rented successfully!");
+                                        oContext.refresh();
+                                    },
+                                    function (oError) {
+                                        oView.setBusy(false);
+                                        MessageToast.show("Error: " + oError.getMessage());
+                                    }
+                                );
+                            }
+                        }
+                    }
                 );
             },
 
             onTilePress: function (oEvent) {
-                const sTitle = oEvent.getSource().getHeader();
-                MessageToast.show("Technical specification: " + sTitle);
+                MessageToast.show("Technical specification: " + oEvent.getSource().getHeader());
             },
 
             onViewDetails: function (oEvent) {
                 const oCarID = oEvent.getSource().getBindingContext().getProperty("ID");
-                this.getRouter().navTo("Details", {
-                    carID: oCarID
-                });
+                this.getRouter().navTo("Details", { carID: oCarID });
             }
         });
     }
--- app/webapp/controller/Home.controller.js
+++ app/webapp/controller/Home.controller.js
@@ -1,9 +1,10 @@
 sap.ui.define(
     [
         "com/moyo/demo/caplugins/controller/BaseController",
-        "sap/m/MessageToast"
+        "sap/m/MessageToast",
+        "sap/m/MessageBox"
     ],
-    function (BaseController, MessageToast) {
+    function (BaseController, MessageToast, MessageBox) {
         "use strict";
 
         return BaseController.extend("com.moyo.demo.caplugins.controller.Home", {
@@ -26,24 +27,37 @@ sap.ui.define(
                 const oSource = oEvent.getSource();
                 const oContext = oSource.getBindingContext();
                 const sCarID = oContext.getProperty("ID");
+                const sBrand = oContext.getProperty("brand");
+                const sModel = oContext.getProperty("model");
                 const oModel = this.getModel();
 
-                oSource.setBusy(true);
+                MessageBox.confirm(
+                    "Are you sure you want to rent the " + sBrand + " " + sModel + "?",
+                    {
+                        title: "Confirm Rental",
+                        actions: [MessageBox.Action.YES, MessageBox.Action.NO],
+                        emphasizedAction: MessageBox.Action.YES,
+                        onClose: function (sAction) {
+                            if (sAction === MessageBox.Action.YES) {
+                                oSource.setBusy(true);
 
-                const oAction = oModel.bindContext("/rentCar(...)");
-                oAction.setParameter("carID", sCarID);
+                                const oAction = oModel.bindContext("/rentCar(...)");
+                                oAction.setParameter("carID", sCarID);
 
-                oAction.execute().then(
-                    function () {
-                        oSource.setBusy(false);
-                        MessageToast.show("Car rented successfully!");
-                        // Refresh the whole /Cars list so status updates everywhere
-                        oModel.refresh();
-                    }.bind(this),
-                    function (oError) {
-                        oSource.setBusy(false);
-                        MessageToast.show("Error: " + oError.getMessage());
-                    }.bind(this)
+                                oAction.execute().then(
+                                    function () {
+                                        oSource.setBusy(false);
+                                        MessageToast.show(sBrand + " " + sModel + " rented successfully!");
+                                        oModel.refresh();
+                                    },
+                                    function (oError) {
+                                        oSource.setBusy(false);
+                                        MessageToast.show("Error: " + oError.getMessage());
+                                    }
+                                );
+                            }
+                        }
+                    }
                 );
             }
         });
